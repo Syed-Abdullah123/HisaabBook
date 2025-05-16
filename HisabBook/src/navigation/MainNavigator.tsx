9bf87fea47610b10d0f15fc5993ad11789608b3e
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-05-16 18:44:29 +0500

    defined the screens as stack in here

--- HisabBook/src/navigation/MainNavigator.tsx
+++ HisabBook/src/navigation/MainNavigator.tsx
@@ -1,13 +1,20 @@
 import TabNavigator from "./TabNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import WelcomeScreen from "../screens/WelcomeScreen";
+import OtpScreen from "../screens/OtpScreen";
+import CreateAccountScreen from "../screens/CreateAccountScreen";
+import BuisnessNameScreen from "../screens/BuisnessNameScreen";
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Create" component={CreateAccountScreen} />
+        <Stack.Screen name="Otp" component={OtpScreen} />
+        <Stack.Screen name="BuisnessName" component={BuisnessNameScreen} />
         <Stack.Screen name="Tab" component={TabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
