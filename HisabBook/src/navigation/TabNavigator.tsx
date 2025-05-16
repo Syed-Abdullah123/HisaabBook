b87cef9be603d7bbcca9099493c5a9f22523af64
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-05-16 18:44:49 +0500

    contains tab screens here

--- HisabBook/src/navigation/TabNavigator.tsx
+++ HisabBook/src/navigation/TabNavigator.tsx
@@ -1,15 +1,86 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import WelcomeScreen from "../screens/WelcomeScreen";
+import HomeScreen from "../screens/HomeScreen";
+import AddContact from "../screens/AddContactScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import { useRoute } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { View, StyleSheet, Pressable } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
+  const route = useRoute();
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Welcome" component={WelcomeScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2F51FF",
+        tabBarInactiveTintColor: "#888",
+        tabBarStyle: {
+          paddingBottom: 15,
+          // paddingTop: 5,
+        },
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          backgroundColor: "transparent",
+        },
+        tabBarButton: (props) => (
+          <Pressable {...props} android_ripple={{ color: "transparent" }} />
+        ),
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AddContact"
+        component={AddContact}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <View style={styles.addButton}>
+              <Ionicons name="add" size={32} color="#FFFFFF" />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  addButton: {
+    backgroundColor: "#2F51FF",
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+});
 
 export default TabNavigator;
