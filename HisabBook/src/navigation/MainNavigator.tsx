af748e7336129940ca13e8a17181c962dc87935e
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-05-27 18:55:51 +0500

    conditional rendering screens based on user authentication

--- HisabBook/src/navigation/MainNavigator.tsx
+++ HisabBook/src/navigation/MainNavigator.tsx
@@ -1,10 +1,13 @@
-import TabNavigator from "./TabNavigator";
+import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthContext } from "../context/AuthProvider";
+
 import WelcomeScreen from "../screens/WelcomeScreen";
 import OtpScreen from "../screens/OtpScreen";
 import CreateAccountScreen from "../screens/CreateAccountScreen";
 import BuisnessNameScreen from "../screens/BuisnessNameScreen";
+import TabNavigator from "./TabNavigator";
 import ContactDetailsScreen from "../screens/ContactDetailsScreen";
 import EntryFormScreen from "../screens/EntryFormScreen";
 import ContactHistoryScreen from "../screens/ContactHistoryScreen";
@@ -13,24 +16,44 @@ import SplashScreen from "../screens/SplashScreen";
 
 const Stack = createNativeStackNavigator();
 
+const AuthStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Splash" component={SplashScreen} />
+    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="Create" component={CreateAccountScreen} />
+    <Stack.Screen name="Otp" component={OtpScreen} />
+    <Stack.Screen name="BuisnessName" component={BuisnessNameScreen} />
+  </Stack.Navigator>
+);
+
+const OnboardingStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="BuisnessName" component={BuisnessNameScreen} />
+  </Stack.Navigator>
+);
+
+const AppStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Tab" component={TabNavigator} />
+    <Stack.Screen name="ContactDetails" component={ContactDetailsScreen} />
+    <Stack.Screen name="EntryForm" component={EntryFormScreen} />
+    <Stack.Screen name="ContactHistory" component={ContactHistoryScreen} />
+    <Stack.Screen name="AddNewContact" component={AddNewContactScreen} />
+  </Stack.Navigator>
+);
+
 const MainNavigator = () => {
+  const { user, isOnboardingComplete } = useContext(AuthContext);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName="Splash"
-      >
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Create" component={CreateAccountScreen} />
-        <Stack.Screen name="Otp" component={OtpScreen} />
-        <Stack.Screen name="BuisnessName" component={BuisnessNameScreen} />
-        <Stack.Screen name="Tab" component={TabNavigator} />
-        <Stack.Screen name="ContactDetails" component={ContactDetailsScreen} />
-        <Stack.Screen name="EntryForm" component={EntryFormScreen} />
-        <Stack.Screen name="ContactHistory" component={ContactHistoryScreen} />
-        <Stack.Screen name="AddNewContact" component={AddNewContactScreen} />
-      </Stack.Navigator>
+      {!user ? (
+        <AuthStack />
+      ) : !isOnboardingComplete ? (
+        <OnboardingStack />
+      ) : (
+        <AppStack />
+      )}
     </NavigationContainer>
   );
 };
