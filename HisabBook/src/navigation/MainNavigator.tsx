6140390cc06bb254fb3cacda22509691b61275c9
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-16 18:50:41 +0500

    modified the stack navigators to better handle navigation

--- HisabBook/src/navigation/MainNavigator.tsx
+++ HisabBook/src/navigation/MainNavigator.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthContext } from "../context/AuthProvider";
 
+// Auth Screens
 import WelcomeScreen from "../screens/WelcomeScreen";
 import OtpScreen from "../screens/OtpScreen";
 import CreateAccountScreen from "../screens/CreateAccountScreen";
@@ -18,24 +19,20 @@ import DeletedItemsScreen from "../screens/DeletedItemsScreen";
 
 const Stack = createNativeStackNavigator();
 
+// Auth Stack Navigator
 const AuthStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Splash" component={SplashScreen} />
     <Stack.Screen name="Welcome" component={WelcomeScreen} />
     <Stack.Screen name="Create" component={CreateAccountScreen} />
     <Stack.Screen name="Otp" component={OtpScreen} />
-    <Stack.Screen name="BuisnessName" component={BuisnessNameScreen} />
   </Stack.Navigator>
 );
 
-const OnboardingStack = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="BuisnessName" component={BuisnessNameScreen} />
-  </Stack.Navigator>
-);
-
+// Main App Stack Navigator (includes both onboarding and main app screens)
 const AppStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="BuisnessName" component={BuisnessNameScreen} />
     <Stack.Screen name="Tab" component={TabNavigator} />
     <Stack.Screen name="ContactDetails" component={ContactDetailsScreen} />
     <Stack.Screen name="EntryForm" component={EntryFormScreen} />
@@ -46,18 +43,13 @@ const AppStack = () => (
   </Stack.Navigator>
 );
 
+// Main Navigator
 const MainNavigator = () => {
   const { user, isOnboardingComplete } = useContext(AuthContext);
 
   return (
     <NavigationContainer>
-      {!user ? (
-        <AuthStack />
-      ) : !isOnboardingComplete ? (
-        <OnboardingStack />
-      ) : (
-        <AppStack />
-      )}
+      {!user ? <AuthStack /> : <AppStack />}
     </NavigationContainer>
   );
 };
