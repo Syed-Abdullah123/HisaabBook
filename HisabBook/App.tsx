5b81465bb83df8efab98d38713aa1886323d641b
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-18 14:46:13 +0500

    fix: wrapped the entry point with SafeAreaProvider

--- HisabBook/App.tsx
+++ HisabBook/App.tsx
@@ -1,10 +1,13 @@
 import MainNavigator from "./src/navigation/MainNavigator";
 import { AuthProvider } from "./src/context/AuthProvider";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <MainNavigator />
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <MainNavigator />
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
